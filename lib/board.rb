c7e6544c960ccb021d7c24a097448c033cded8df
Noak Pettersson <noake12@gmail.com>
2022-03-08 16:18:57 +0100

    added visited variable to board

--- lib/board.rb
+++ lib/board.rb
@@ -5,10 +5,21 @@ require_relative 'knight'
 # board class
 class Board
 
+  @visited = []
+
+  def build_graph
+
+  end
+
+  # Find all children to a knight(parent)
   def create_children(parent)
+
     parent.possible_moves.each do |location|
-      child = Knight.new(location)
-      parent.children << child
+      unless @visited.include? location
+        child = Knight.new(location)
+        parent.children << child
+        visited << location
+      end
     end
   end
 
