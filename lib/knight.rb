96be27c2349cf8796ee59fc1bf3efc53f91b464d
Noak Pettersson <noake12@gmail.com>
2022-03-08 15:52:20 +0100

    initialize and method for checking possible moves

---
+++ lib/knight.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: false
+
+# Knight class.
+class Knight
+  attr_accessor :location, :moves, :children
+
+  def initialize(location)
+    @location = location
+    @moves = possible_moves
+    @children = []
+  end
+
+  def possible_moves
+    all_moves = [
+      [1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]
+    ]
+    result = []
+
+    all_moves.each do |move|
+      new_x = @location[0] + move[0]
+      new_y = @location[1] + move[1]
+      result << [new_x, new_y] if new_x.between?(0, 7) && new_y.between?(0, 7)
+    end
+    result
+  end
+end
+
+k = Knight.new([0, 0])
+p k.moves
+puts "\n\n"
+
+k = Knight.new([3, 3])
+p k.moves
