1b6aeb4d2470ed6c94c159c4d5f19d0d3e7f931b
Noak Pettersson <noake12@gmail.com>
2022-03-09 03:28:46 +0100

    create_graph in Game class is done

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,26 @@
 # frozen_string_literal: false
 
+require_relative 'knight'
+require_relative 'board'
+
 # Game class
 class Game
 
-  def create_graph; end
+  def knight_moves(start, destination)
+    graph = create_graph(start)
+    # Find the path
+    # Display the path
+  end
+
+  # Create the graph, starting from the start location
+  def create_graph(start)
+    start = Knight.new(start)
+    start.create_children
+    Board.new(start)
+  end
 
   def show_path; end
 
-  def knight_moves(start, destination); end
 
+  
 end
