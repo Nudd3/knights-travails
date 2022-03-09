51b8f4296991d733d3470a2bc2f47181e33fdb70
Noak Pettersson <noake12@gmail.com>
2022-03-09 03:38:25 +0100

    Added bfs in Board. Needs testing

--- lib/board.rb
+++ lib/board.rb
@@ -10,4 +10,17 @@ class Board
     @root = start
     @visited = []
   end
+
+  def find_path_bfs(destination)
+    queue = [@root]
+    until queue.empty?
+      current = queue.shift
+      return current if current.location == destination
+
+      @visited.push(current)
+      current.children.each do |child|
+        queue << child unless @visited.include?(child)
+      end
+    end
+  end
 end
