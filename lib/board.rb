c6df8a7974702665be5bc159275e5db111ff5791
Noak Pettersson <noake12@gmail.com>
2022-03-09 02:54:48 +0100

    Reset knight_moves, problem in create_graph

--- lib/board.rb
+++ lib/board.rb
@@ -9,46 +9,23 @@ class Board
   # Find all children to a knight(parent)
   def create_children(parent)
 
-    parent.possible_moves.each do |location|
+    parent.moves.each do |location|
 
       child = Knight.new(location)
       parent.children << child
     end
   end
 
-  def build_graph(parent)
-    return if parent.nil?
+  def build_graph(start)
 
+    start = Knight.new(start)
+    start.create_children
+    
     create_children(parent)
 
     parent.children.each do |child|
       create_children(parent)
       build_graph(child)
     end
-  end
-
-  def find_path(start, searched_location, path = [])
-    queue = []
-    queue << start
-
-    until queue.empty?
-      node = queue.shift
-      path << node
-      if node.location == searched_location
-        puts "HELLO"
-        return path
-      end
-
-      start.children.each do |child|
-        queue << child
-      end
-    end
-    return path
-  end
-
-  def knight_moves(start, destination)
-    @knight = Knight.new(start)
-    build_graph(@knight)
-    path = find_path(@knight, destination)
   end
 end
